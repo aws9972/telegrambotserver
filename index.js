a3afdb7165390953e5b4548b34973c65deea58fa
aws997 <aws.basim97@gmail.com>
2021-04-16 13:47:31 +0300

    make it better

--- index.js
+++ index.js
@@ -23,6 +23,19 @@ app.get("/sendm",(req,res)=>{
     res.send("Hello World")
     });
 
+    // Matches "/echo [whatever]"
+bot.onText(/\/echo (.+)/, (msg, match) => {
+    // 'msg' is the received Message from Telegram
+    // 'match' is the result of executing the regexp above on the text content
+    // of the message
+  
+    const chatId = msg.chat.id;
+    const resp = match[1]; // the captured "whatever"
+  
+    // send back the matched "whatever" to the chat
+    bot.sendMessage(chatId, resp);
+  });
+  
 
     // Listen for any kind of message. There are different kinds of
 // messages.
