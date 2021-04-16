c51a78ef4e5dd55553089b79fd18123d3fb1b6e8
aws997 <aws.basim97@gmail.com>
2021-04-16 15:42:48 +0300

    make it better

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express =require("express");
 const app=express();
 
+const https = require('https')
+const axios = require('axios').default;
+
 const TelegramBot = require('node-telegram-bot-api');
 
 // replace the value below with the Telegram token you receive from @BotFather
@@ -19,8 +22,19 @@ app.listen(port,()=>{
     console.log(port)
 });
 
-app.get("/sendm",(req,res)=>{
-    res.send("Hello World")
+app.get("/sendm",(req1,res1)=>{
+
+    axios.get(
+        'https://api.ethermine.org/miner/0x103836D8BA826c49C1B41b8b4bdED96dd26066C4/currentStats'
+        ).then((s)=>{
+            var unpaid=s.data.data.unpaid;
+            var paid2=`${unpaid}`.substring(0,4)/100000
+           
+        })
+
+ bot.sendMessage("61159086",`${paid2}`)
+    res1.send("Hello World")
+    
     });
 
     // Matches "/echo [whatever]"
@@ -35,13 +49,13 @@ bot.onText(/\/echo (.+)/, (msg, match) => {
     // send back the matched "whatever" to the chat
     bot.sendMessage(chatId, resp);
   });
-  
+
 
     // Listen for any kind of message. There are different kinds of
 // messages.
 bot.on('message', (msg) => {
     const chatId = msg.chat.id;
-  
+  console.log(chatId)
     // send a message to the chat acknowledging receipt of their message
     bot.sendMessage(chatId, 'Received your message');
   });
